be0517984237440ebbed3e6f98165179e7d62d33
LAPTOP-8P0SVNS7\CMH <1360041885@qq.com>
2018-12-28 22:42:31 +0800

    六

---
+++ chapter6/6.42.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double xa=0;
+    double ya=0;
+    double xb=0;
+    double yb=0;
+    double c=0;
+    double d=0;
+    cout << "Enter the Xa:";
+    cin >> xa ;
+cout << "Enter the Ya:";
+cin >> ya ;
+cout << "Enter the Xb:";
+cin >> xb;
+cout << "Enter the Yb:";
+cin >> yb;
+
+
+ d=(xa-xb)*(xa-xb)+(ya-yb)*(ya-yb);
+
+
+   cout << d << endl;
+
+}
